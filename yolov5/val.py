6c42455a0e1df4aaed37183921fd9bb5b1cbfe1b
chrise96 <chrise96@hotmail.com>
2023-09-20 16:36:07 +0200

    Feedback

--- yolov5/val.py
+++ yolov5/val.py
@@ -41,6 +41,7 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 from baas_utils.database_handler import DBConfigSQLAlchemy
 from baas_utils.database_tables import DetectionInformation, ImageProcessingStatus, BatchRunInformation
 from baas_utils.date_utils import extract_upload_date, get_current_time
+from baas_utils.error_handling import exception_handler
 
 from models.common import DetectMultiBackend
 from utils.callbacks import Callbacks
@@ -158,51 +159,6 @@ def process_batch(detections, labels, iouv):
             correct[matches[:, 1].astype(int), i] = True
     return torch.tensor(correct, dtype=torch.bool, device=iouv.device)
 
-
-def exception_handler(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            # Handle the exception here
-            print(f"Exception caught: {e}")
-
-            db_username = kwargs.get('db_username', '')
-            db_name = kwargs.get('db_name', '')
-            db_hostname = kwargs.get('db_hostname', '')
-            run_id = kwargs.get('run_id', 'default_run_id')
-            trained_yolo_model = kwargs.get('weights', 'default')
-            start_time = kwargs.get('start_time', '')
-            # Check if 'skip_evaluation' is provided as an argument in kwargs, and if not, default to False
-            skip_evaluation = kwargs.get('skip_evaluation', False)
-
-            if skip_evaluation:
-                # Validate if database credentials are provided
-                if not db_username or not db_name or not db_hostname:
-                    raise ValueError('Please provide database credentials.')
-
-                # Create a DBConfigSQLAlchemy object
-                db_config = DBConfigSQLAlchemy(db_username, db_hostname, db_name)
-                # Create the database connection
-                db_config.create_connection()
-
-                # Perform database operations using the 'session'
-                # The session will be automatically closed at the end of this block
-                with db_config.managed_session() as session:
-                    # Create an instance of BatchRunInformation
-                    batch_info = BatchRunInformation(run_id=run_id,
-                                                     start_time=start_time,
-                                                     end_time=get_current_time(),
-                                                     trained_yolo_model=trained_yolo_model,
-                                                     success=False,
-                                                     error_code=str(e))
-
-                    # Add the instance to the session
-                    session.add(batch_info)
-
-            # Re-raise the exception
-            raise e
-    return wrapper
 
 @exception_handler
 @smart_inference_mode()
