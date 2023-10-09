579b7c62c0bca1e6bc3dee050dbf680f79092df6
Sebastian Davrieux <s.davrieux@amsterdam.nl>
2023-10-09 14:52:24 +0200

    Fixing the paths

--- yolov5/utils/general.py
+++ yolov5/utils/general.py
@@ -35,8 +35,6 @@ import pkg_resources as pkg
 import torch
 import torchvision
 import yaml
-from blurring_as_a_service.settings.settings import BlurringAsAServiceSettings
-from blurring_as_a_service.utils.logging_handler import setup_azure_logging_from_config
 
 from utils import TryExcept, emojis
 from utils.downloads import curl_download, gsutil_getsize
@@ -147,7 +145,9 @@ def set_logging(name=LOGGING_NAME, verbose=True):
                 'propagate': False,}}})
 
 
-LOGGER = setup_azure_logging_from_config()
+set_logging(LOGGING_NAME)  # run before defining LOGGER
+LOGGER = logging.getLogger(LOGGING_NAME)  # define globally (used in train.py, val.py, detect.py, etc.)
+
 
 if platform.system() == 'Windows':
     for fn in LOGGER.info, LOGGER.warning:
