2e7878097a928a6a24f2fa195a2d566ac3fccff9
chrise96 <chrise96@hotmail.com>
2023-11-05 10:16:19 +0100

    Update val.py

--- yolov5/val.py
+++ yolov5/val.py
@@ -268,7 +268,7 @@ def run(
 
         if skip_evaluation:
             # Define the processing statuses
-            processing_statuses = ['processed']
+            processing_statuses = ['inprogress', 'processed']
 
             # Perform database operations using the 'session'
             # The session will be automatically closed at the end of this block
