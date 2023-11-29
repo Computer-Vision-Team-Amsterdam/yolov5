10129cb70ad393f7021c31d0a7f43a2cc63f200a
Andrea Lombardo <60581994+imandrealombardo@users.noreply.github.com>
2023-11-29 17:12:30 +0100

    test

--- yolov5/baas_utils/database_handler.py
+++ yolov5/baas_utils/database_handler.py
@@ -73,8 +73,8 @@ class DBConfigSQLAlchemy:
             self.session_maker = sessionmaker(bind=self.engine, autoflush=False, autocommit=False)
             
             # Check the connect_args in the engine's URL
-            timeout = self.engine.url.translate_connect_args().get('connect_timeout')
-            print(f'Timeout: {timeout}')
+            args, kwargs = self.engine.dialect.create_connect_args(self.engine.url)
+            print(f'Timeout: {args}, {kwargs}')
 
             LOGGER.info(f"Successfully created database sessionmaker.")
 
